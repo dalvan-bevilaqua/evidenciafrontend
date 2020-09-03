962fa547ddd04d402f3d0ca365293e4b20bfe0b1
dalvan bevilaqua <dalvan.bevilaqua@hotmail.com>
2020-09-02 21:40:21 -0300

    relatorio de vendas

--- administrator/html/assets/js/scripts/form-plugins.js
+++ administrator/html/assets/js/scripts/form-plugins.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // Select 2
     $(".select2_demo_1").select2();
@@ -61,7 +61,7 @@ $(document).ready(function(){
         singleDatePicker: true,
         showDropdowns: true
     });
-    $(function() {
+    $(function () {
         var start = moment().subtract(29, 'days');
         var end = moment();
         function cb(start, end) {
@@ -71,12 +71,12 @@ $(document).ready(function(){
             startDate: start,
             endDate: end,
             ranges: {
-               'Today': [moment(), moment()],
-               'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-               'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-               'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-               'This Month': [moment().startOf('month'), moment().endOf('month')],
-               'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
             }
         }, cb);
         cb(start, end);
@@ -86,17 +86,17 @@ $(document).ready(function(){
     $('.clockpicker').clockpicker();
 
     // Jquery minicolors
-    $('.minicolors').each(function(){
+    $('.minicolors').each(function () {
         $(this).minicolors({
-            theme:"bootstrap",
-            control:$(this).attr("data-control")||"hue",
+            theme: "bootstrap",
+            control: $(this).attr("data-control") || "hue",
             format: $(this).attr('data-format') || 'hex',
-            opacity:$(this).attr("data-opacity"),
+            opacity: $(this).attr("data-opacity"),
             swatches: $(this).attr('data-swatches') ? $(this).attr('data-swatches').split('|') : [],
             position: $(this).attr('data-position') || 'bottom left',
         });
     });
-    
+
     // jQuery Knob
     $(".dial").knob();
 
@@ -154,7 +154,7 @@ $(document).ready(function(){
 
     // Maxlength Control
     $('[maxlength]').maxlength();
-    
+
     // Touchspin1
     $(".touchspin1").TouchSpin({
         buttondown_class: 'btn btn-secondary',
--- assets/controller/vendasRelatorio.js
+++ assets/controller/vendasRelatorio.js
@@ -13,9 +13,42 @@ app.controller("vendasRelatorio", ['$scope', '$http', 'Crud', 'Upload', function
     $scope.vendas = [];
     $scope.produtosVenda = [];
     $scope.valorVenda = 0;
+    $scope.valorData = "";
+
 
     $scope.getAllVendas = function () {
         Crud.getAll("venda", function (res) {
+            $scope.vendas = res;
+        })
+    }
+    $scope.getDate = function (callback) {
+        var data = $('#daterange_2').val();
+
+        if (data == '') {
+            return callback('');
+        }
+
+        data = data.split(" - ");
+        var dataInicial = data[0].split("/")
+        var dataFinal = data[1].split("/")
+        dataInicial = dataInicial[2] + '-' + dataInicial[1] + '-' + dataInicial[0]
+        dataFinal = dataFinal[2] + '-' + dataFinal[1] + '-' + dataFinal[0]
+        return callback("dataInicial=" + dataInicial + "&dataFinal=" + dataFinal);
+    }
+    $scope.imprimirRelatorio = function () {
+        var url = null;
+        $scope.getDate(function (retornoCalback) {
+            url = "venda/relatorioVendas?" + retornoCalback;
+        })
+        Crud.relatorio(url);
+    }
+
+    $scope.buscar = function () {
+        var url = null;
+        $scope.getDate(function (retornoCalback) {
+            url = "venda?" + retornoCalback;
+        })
+        Crud.getById(url, function (res) {
             $scope.vendas = res;
         })
     }
@@ -25,10 +58,6 @@ app.controller("vendasRelatorio", ['$scope', '$http', 'Crud', 'Upload', function
             $scope.produtosVenda = res;
         })
         $scope.valorVenda = valor;
-    }
-
-    $scope.imprimirRelatorio = function () {
-        Crud.relatorio("venda/relatorioVendas");
     }
 
     $scope.getAllVendas();
